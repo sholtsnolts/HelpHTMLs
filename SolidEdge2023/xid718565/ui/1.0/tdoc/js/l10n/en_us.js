77d37a69170e00ee9e31bbb6b3f99fac039c722a
sholtsnolts <64537016+sholtsnolts@users.noreply.github.com>
2026-07-09 14:51:56 -0400

    Fixed stuff

---
+++ SolidEdge2023/xid718565/ui/1.0/tdoc/js/l10n/en_us.js
@@ -0,0 +1,14 @@
+window.l10n = {
+    "back_to_collection": "Back to collection",
+    "privacy_footer": "Privacy Policy",
+    "terms_footer": "Terms of use",
+    "siemens_inc": "Siemens Industry Software, Inc.",
+    "lms_start_where_last":"Do you want to start where you left off last time?",
+    "user_preferences": "User Preferences",
+    "restore_defaults": "Restore defaults",
+    "save": "Save",
+    "settings": "Settings",
+    "searchPlaceholder": "Enter your search word here...",
+    "searchResults": "Found {0} results for {1}",
+    "copy_button": "Copy"
+};
